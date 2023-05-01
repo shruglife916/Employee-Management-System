176c6f71d178dff003904a7f9cb8588d9e4292cc
shruglife916 <79869470+shruglife916@users.noreply.github.com>
2023-05-01 10:09:44 -0500

    Add files via upload

---
+++ spring-backend/src/main/java/com/springboot/second/app/SpringbootSecondApp1Application.java
@@ -0,0 +1,13 @@
+package com.springboot.second.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecondApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecondApp1Application.class, args);
+	}
+
+}
---
+++ spring-backend/src/test/java/com/springboot/second/app/SpringbootSecondApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.second.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecondApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
